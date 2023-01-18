4df2142a951062cdf0ba9504c9b8b496652d6729
RynDaniel <ryan@clarusway.com>
2023-01-18 22:02:56 +0300

    second lesson commit

--- app.js
+++ app.js
@@ -31,4 +31,49 @@ addBtn.addEventListener("click", (e) => {
         completed : false, //status
         text : todoInput.value //userInput
     }
+
+    createTodo(newTodo);
+    
+    todoList.push(newTodo);
+    //localStorage todoList Update
+    //localStorage vs. SessionStorage vs. Cookies
+    //!!!!!!!!stringify!!!!!!!!!
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+    //event.target vs. event.currentTarget
+    e.target.closest("form").reset();
 })
+
+const createTodo = (newTodo) =>{
+    //todo item creation
+    //alert("item was added");
+    //obj. dest. (ES6 => JS'e kazandırılan yapılar??)
+    const { id, completed, text } = newTodo;
+
+    //create li
+    const li = document.createElement("li");
+    li.setAttribute("id", id);
+
+    //add class with completed(status)
+    completed ? li.classList.add("checked") : "";
+
+    //create check icon
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "fas fa-check");
+    //append vs. appendChild
+    li.append(icon);
+
+    //create item text
+    const p = document.createElement("p");
+    p.innerText = text;
+    li.appendChild(p);
+
+    //create remove icon
+    const removeIcon = document.createElement("i");
+    removeIcon.setAttribute("class", "fas fa-trash");
+    li.append(removeIcon);
+
+    console.log(li);
+
+    //append li to ul
+    todoUl.append(li);
+}
